d496e54c74faee42686408106dc12c5c915bd814
Tesfawen <101813048+Tesfawen@users.noreply.github.com>
2022-07-07 00:25:32 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
+import sys
 
-num1 = 0
-length = len(argv)
-for i in range(1, length):
-    num1 += int(argv[i])
-print('{}'.format(num1))
+if __name__ == '__main__':
+    av = sys.argv
+    l_av = len(av)
+    sum = 0
+
+    if l_av > 1:
+        for i in range(1, l_av):
+            sum += int(av[i])
+
+    print(sum)
