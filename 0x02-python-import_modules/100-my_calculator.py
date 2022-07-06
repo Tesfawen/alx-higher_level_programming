e8590c6aab37f22b222b3d2550a122c13727c3bf
Tesfawen <101813048+Tesfawen@users.noreply.github.com>
2022-07-07 00:31:00 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif sys.argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    """
+    Handles basic operations
+    Performs basic operations like addition, substraction,
+    multiplication and division between two numbers.
+    The program will execute an operation between two numbers
+    selected by the operator sent to the program.
+    """
+    l_av = len(argv) - 1
+
+    if l_av == 3:
+        operator = argv[2]
+        num_a = int(argv[1])
+        num_b = int(argv[3])
+        if operator == '+':
+            res = add(num_a, num_b)
+            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '-':
+            res = sub(num_a, num_b)
+            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '*':
+            res = mul(num_a, num_b)
+            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '/':
+            res = div(num_a, num_b)
+            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
