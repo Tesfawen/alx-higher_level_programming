03c308dde773b175e6b4b88efc89ddb9d6710b50
Tesfawen <101813048+Tesfawen@users.noreply.github.com>
2022-07-26 19:57:48 +0300

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,9 +14,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """
         Checks the parameters and initializes some values
-        Args:        
-
- width (:obj:`int`, optional): The width of the Rectangle.
+        Args:
+            width (:obj:`int`, optional): The width of the Rectangle.
             height (:obj:`int`, optional): The height of the Rectangle.
         """
 
